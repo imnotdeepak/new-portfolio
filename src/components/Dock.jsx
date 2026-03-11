78397ad07516a77bd2e82cba7175aaff253169da
imnotdeepak <rambutanfruitjuice@gmail.com>
2026-03-11 16:29:23 -0400

    added the dock at the bottom

---
+++ src/components/Dock.jsx
@@ -0,0 +1,37 @@
+import { dockApps } from "#constants";
+import React from "react";
+import { Tooltip } from "react-tooltip";
+
+const Dock = () => {
+  const toggleApp = (app) => {};
+  return (
+    <section id="dock">
+      <div className="dock-container">
+        {dockApps.map(({ id, icon, name, canOpen }) => (
+          <div key={id} className="relative flex justify-center">
+            <button
+              type="button"
+              className="dock-icon"
+              aria-label={name}
+              data-tooltip-id="dock-tooltip"
+              data-tooltip-content={name}
+              data-tooltip-delay-show={150}
+              disabled={!canOpen}
+              onClick={() => toggleApp({ id, canOpen })}
+            >
+              <img
+                src={`/images/${icon}`}
+                alt={name}
+                loading="lazy"
+                className={canOpen ? "" : "opacity-60"}
+              />
+            </button>
+          </div>
+        ))}
+        <Tooltip id="dock-tooltip" place="top" className="tooltip" />
+      </div>
+    </section>
+  );
+};
+
+export default Dock;
